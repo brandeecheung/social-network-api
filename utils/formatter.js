0fd79c6366f44131647a795051d8a7e929473f62
brandeecheung <164966810+brandeecheung@users.noreply.github.com>
2024-08-10 21:09:08 -0500

    remove data and seed, add time to formatter

--- utils/formatter.js
+++ utils/formatter.js
@@ -1,5 +1,5 @@
 const dateFormat = (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at ${new Date(date).getHours()}:${new Date(date).getMinutes()}:${new Date(date).getSeconds()}`;
 }
 
 module.exports = { dateFormat };
